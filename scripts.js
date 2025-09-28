c83ceb00b002324b23860f964b5196d282e92ef1
SimbaSimbiri <simbiriisaacray@gmail.com>
2025-09-28 21:57:38 +0300

    removed slideshow

--- scripts.js
+++ scripts.js
@@ -46,24 +46,6 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
-// Slideshow for About Section
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function showSlides(n) {
-  let slides = document.getElementsByClassName('mySlides');
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
-  }
-  slides[slideIndex - 1].style.display = 'block';
-}
-
 // Typewriter effect for the hero section animated text
 const typeTexts = [
   "the App Dev",
